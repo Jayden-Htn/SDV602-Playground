e8ba58fc670f32e95094b1f54df57c07fb598f82
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-07-24 09:00:33 +1200

    Finish chapter 1

---
+++ Week-1/linkedin_ch1.py
@@ -0,0 +1,63 @@
+# Following along with the linkedin course: Python for Non-Programmers by Nick Walter.
+# I have a strong background in Python, so this is a quick refresher for me.
+
+
+# <---- 1: Python Basics ---->
+
+# 1.1
+wallet = 41
+print(wallet)
+wallet = 32
+print(wallet)
+
+day = 23
+
+
+# 1.2
+day = 21
+temp = -15
+weight = 79.7621
+print(3 + 6)
+print(day + 3)
+print(weight * 2)
+print(wallet / 2)
+print(temp - 2)
+
+
+# 1.3
+shirt = 'blue'
+print(shirt)
+store = "Todd's Super Shirt Store"
+rating = "The movie was \"super\" great"
+
+
+# 1.4
+date = 21
+day_name = "Monday"
+print(f"Today is {day} the {date}")
+
+
+# 1.5
+light_on = True
+light_2_on = False
+print(light_on, light_2_on)
+
+if light_on:
+    print("The light is on")
+print("Light")
+
+
+# 1.6
+light_on = True
+if light_on:
+    print("The light is on")
+else:
+    print("The light is off")
+
+if weight < 90:
+    print("You are underweight")
+
+age = 19
+if age >= 18:
+    print("You are an adult")
+
