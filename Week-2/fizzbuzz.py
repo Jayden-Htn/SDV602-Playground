12f7e8cb57d3d3fa8340d735b51640c2c72d9748
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-07-28 15:07:56 +1200

    rewrite fizzbuzz with a function and in range

--- Week-2/fizzbuzz.py
+++ Week-2/fizzbuzz.py
@@ -1,13 +1,15 @@
-x = 1
 
-while x <= 50:
-    if x % 3 == 0 and x % 5 == 0:
-        print('FizzBuzz')
-    elif x % 3 == 0:
-        print('Fizz')
-    elif x % 5 == 0:
-        print('Buzz')
-    else:
-        print(x)
-    x += 1
+def fizzbuzz(up_to):
+    for i in range(1, up_to + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+        i += 1
+
+fizzbuzz(20)
     
