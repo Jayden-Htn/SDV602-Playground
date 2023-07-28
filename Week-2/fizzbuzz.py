170e4f41f4846d625158c7ec8b58911cbb7db77b
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-07-28 15:41:40 +1200

    rewrite fizzbuzz for exercise 4

--- Week-2/fizzbuzz.py
+++ Week-2/fizzbuzz.py
@@ -1,19 +1,19 @@
 
-def fizzbuzz(up_to):
+def fizzbuzz(f_num, b_num, up_to):
     i = 1
     list = []
     while i <= up_to:
-        if i % 3 == 0 and i % 5 == 0:
+        if i % f_num == 0 and i % b_num == 0:
             list.append((i, 'FizzBuzz'))
-        elif i % 3 == 0:
+        elif i % f_num == 0:
             list.append((i, 'Fizz'))
-        elif i % 5 == 0:
+        elif i % b_num == 0:
             list.append((i, 'Buzz'))
         else:
             list.append((i, str(i)))
         i += 1
     return list
 
-fizzbuzz_list = fizzbuzz(20)
+fizzbuzz_list = fizzbuzz(3, 5, 20)
 print(fizzbuzz_list)
     
