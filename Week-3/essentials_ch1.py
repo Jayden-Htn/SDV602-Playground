7257a5387aca299f21912bef6f15efbf80b1438b
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-08-03 16:04:14 +1200

    Finished Linkedin essentials chapter 1

---
+++ Week-3/essentials_ch1.py
@@ -0,0 +1,23 @@
+# Following the Linkedin course: Python Essential Training (2022) by Ryan Mitchell.
+# I have a strong background in Python, so this is most a quick refresher for me, but also to gain any small extra pieces of knowledge.
+
+
+# <---- 1: Gearing Up for Python ---->
+
+# 1.3
+import this
+
+
+# 1.4
+
+# Print to terminal
+print('Hello World!')
+
+
+# 1.5
+
+import termcolor as tc # terminal colour
+
+# Print to terminal
+tc.cprint('Hello World!', "red", "on_blue")
+print(tc.colored('colours!', 'green'))
