152128b7e64530b4ab13d8a7b91e63080c757eff
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-09-04 10:12:02 +1200

    Finish Linkedin working with files chapter 1

--- Week-6/working_with_files_ch1.py
+++ Week-6/working_with_files_ch1.py
@@ -82,3 +82,107 @@ if __name__ == "__main__":
     # display_entries_in_directory('Week-6/')
     # display_directories('./')
     # display_files('./')
+
+
+# 1.4: Filter path names with glob module
+
+# import glob # glob = global, searches the current directory for files
+
+def display_png():
+    png_files = glob.glob('*.png') # glob = global, searches the current directory for files
+    # '../**/*.png' = recursive=True, will search all subdirectories
+    
+def find_monster_one():
+    filtered_item = glob.glob('*monster01*') # finds all files with monster01 in the name
+
+def find_in_subdirectory():
+    for item in glob.iglob('**/*monster01*', recursive=True): # iglob = iterator glob, returns an iterator instead of a list
+        print(item)
+
+if __name__ == "__main__":
+    pass
+    # display_png() # because of file structure, won't work
+    # find_monster_one() # because of file structure, won't work
+    # find_in_subdirectory() # large output
+
+
+# 1.5: Recursively list all files in a directory
+
+# import os
+
+# os.walk = generate files names in a directory tree by walking the tree either top-down or bottom-up
+
+def top_down_walk():
+    for dirpath, dirnames, files in os.walk('Week-6'): # returns a generator object
+        print('Current path:', dirpath)
+        print('Sub directories:')
+        for dirname in dirnames:
+            print(dirname)
+        print('Files:')
+        for file in files:
+            print(file)
+        print()
+
+def bottom_up_walk():
+    for dirpath, dirnames, files in os.walk('Week-6', topdown=False): # returns a generator object
+        print('Current path:', dirpath)
+        print('Sub directories:')
+        for dirname in dirnames:
+            print(dirname)
+        print('Files:')
+        for file in files:
+            print(file)
+        print()
+
+if __name__ == "__main__":
+    pass
+    # top_down_walk()
+    # bottom_up_walk()
+
+
+# 1.6 Understand Python's new pathlib module
+
+# from pathlib import Path
+
+def print_directory_path():
+    entries = Path.cwd() # cwd = current working directory
+
+    # specify specifc directory with path, relatve or absolute
+    # entries = Path('Week-6/')
+    # entries = Path.home() # home = home directory of the user
+
+    for entry in entries.iterdir(): # iterdir = creates an iterator of the entries in the directory
+        print(entry.name) # e.g. monster.png
+        print(entry.parent) # parent = parent directory of the entry, e.g. /Users/practice/Week-6
+        print(entry.parent.parent) # parent.parent = parent directory of the parent directory of the entry, e.g. /Users/practice
+        print(entry.stem) # stem = name of the entry without the extension, e.g. monster
+        print(entry.suffix) # suffix = extension of the entry, e.g. .png
+        print(entry.stat()) # stat = statistics objects of the file
+
+if __name__ == "__main__":
+    pass
+    # print_directory_path()
+
+    
+# 1.7: Create directories in Python
+
+import os
+from pathlib import Path
+
+def make_logs_dir():
+    try:
+        os.mkdir('logs/') # mkdir = make directory
+        # can use absolute or relative path
+    except FileExistsError as ex:
+        print('Logs directory already exists')
+    
+def make_output_dir():
+    dir_path = Path('output/')
+    dir_path.mkdir(exist_ok=True) # mkdir = make directory
+
+if __name__ == "__main__":
+    pass
+    # make_logs_dir() # error?
+    make_output_dir() # error?
+
+
