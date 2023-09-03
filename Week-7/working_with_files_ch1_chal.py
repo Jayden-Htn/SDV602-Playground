0bd71da77664bdf3e2af812d9e1045edd6520f93
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-09-04 10:42:18 +1200

    Finish Linkedin working with files chapter 1 challenge

---
+++ Week-7/working_with_files_ch1_chal.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+# my method
+def count_files_scandir(directory):
+    print(directory)
+    count = 0
+    with os.scandir(directory) as entries:
+        for entry in entries:
+            if entry.is_file():
+                count += 1
+                # print("File name:", entry.name)
+            elif entry.is_dir():
+                num = count_files_scandir(entry.path)
+                count += num
+    return count
+
+# alternative method
+def count_files_walk(directory):
+    count = 0
+    for dirpath, dirnames, files in os.walk(directory):
+        for file in files:
+            count += 1
+    return count
+
+# another alternative method
+def count_file_path(directory):
+    count = 0
+    for entry in Path(directory).iterdir():
+        if entry.is_file():
+            count += 1
+        elif entry.is_dir():
+            num = count_file_path(entry.path)
+            count += num
+    return count
+
+if __name__ == "__main__":
+    # scandir option
+    print(count_files_scandir('C:\\Users\\jayde\\Desktop'))
+    # os.walk option
+    print(count_files_walk('C:\\Users\\jayde\\Desktop'))
+
